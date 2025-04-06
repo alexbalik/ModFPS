75deea69643d1d84dbcf08e45ee1f48f76247a47
Alex Balik <92473619+alexbalik@users.noreply.github.com>
2025-04-06 13:32:29 -0400

    First commit. Load pre-made FPS, mod scripts
    At the time of this commit, I modified scripts such that
    health detoriates every frame by default and picking up health
    packs makes you lose heath

---
+++ Assets/FPS/Scripts/Gameplay/HealthPickup.cs
@@ -0,0 +1,28 @@
+﻿using Unity.FPS.Game;
+using UnityEngine;
+
+namespace Unity.FPS.Gameplay
+{
+    public class HealthPickup : Pickup
+    {
+        [Header("Parameters")] [Tooltip("Amount of health to heal on pickup")]
+        public float HealAmount;
+
+        protected override void OnPicked(PlayerCharacterController player)
+        {
+            Health playerHealth = player.GetComponent<Health>();
+            if (playerHealth) //&& playerHealth.CanPickup())
+            {
+                //playerHealth.Heal(HealAmount);
+                PlayPickupFeedback();
+                Destroy(gameObject);
+                //Note: At first you tried to use the "HandleDeath()" method directly, but HandleDeath() is
+                //a private method inside the Health class. Then you made the HandleDeath() method public
+                //which worked, but below is a safer way to access HandleDeath. The statement below accesses
+                //HandleDeath through another already public method - TakeDamage() - that calls HandleDeath at the
+                //end of its body
+                playerHealth.TakeDamage(HealAmount, null);
+            }
+        }
+    }
+}
